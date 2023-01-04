921a4aed01d326678308b9b7014049f53a751a4d
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-01-04 11:16:42 +0530

    adding basic structure

---
+++ mod/svasu/lang/en/svasu.php
@@ -0,0 +1,7 @@
+<?php 
+$string['mode'] = 'Mode';
+$string['modulename'] = 'Svasu Cloud Plugin';
+$string['modulename_help'] = 'Transform your Knowledge, PPT, PDF, Video into Digital Learning (SVASU Scorm). It is a simple to use platform to transform your knowledge into Digital Learning.Instantly Convert Your PowerPoint / PDF / Video Training to Interactive Course';
+$string['modulename_link'] = 'mod/svasu/view';
+$string['modulenameplural'] = 'Svasu Cloud Plugin';
+?>
---
+++ mod/svasu/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_svasu';
+$plugin->release = '0.1.0';
+$plugin->version = 2022122300;
+$plugin->requires = 2019111800;
+$plugin->maturity = MATURITY_ALPHA;
